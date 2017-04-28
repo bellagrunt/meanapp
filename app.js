36ab97891d25cbb8a99c5be5ca12134e038e5689
Matt Bellantoni <mattbellantoni3@gmail.com>
2017-04-28 10:19:33 -0400

    updated authenicate in routs and then added a new config in the config folder called, 'passport.js'

--- app.js
+++ app.js
@@ -34,6 +34,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 //body parser middleware
 app.use(bodyParser.json());
 
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
 app.use('/users', users);
 
 // index route
