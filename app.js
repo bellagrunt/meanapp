e4270c6398946c13f113dd45e1ed3c56a4e4d3e1
Matt Bellantoni <mattbellantoni3@gmail.com>
2017-04-29 20:49:29 -0400

    edited app.js and edited dashboard html

--- app.js
+++ app.js
@@ -47,6 +47,10 @@ app.get('/', (req, res) => {
     res.send('Invalid Endpoint');
 });
 
+app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname, 'public/index/html'));
+});
+
 // start server
 app.listen(port, () => {
     console.log('Server started on port' +port);
